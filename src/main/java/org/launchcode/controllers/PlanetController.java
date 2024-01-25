ed084ef4110f16bc93070b8fa914c4c36d049f4e
Kevin Wiegman <wiegmank@gmail.com>
2024-01-24 22:32:07 -0600

    broken gradle from LC git?

--- src/main/java/org/launchcode/controllers/PlanetController.java
+++ src/main/java/org/launchcode/controllers/PlanetController.java
@@ -1,13 +1,15 @@
 package org.launchcode.controllers;
 
+import org.launchcode.data.Planets;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PlanetController {
 
-    @GetMapping
-    public String displayIndex() {
+    @GetMapping("")
+    public String displayIndex(Model model) {
+        model.addAttribute("planets", Planets.values());
         return "index";
     }
 }
