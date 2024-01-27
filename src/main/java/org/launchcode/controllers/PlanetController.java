803506314d1c458aea4468c2b69e14ace6efdec9
Kevin Wiegman <wiegmank@gmail.com>
2024-01-26 18:24:22 -0600

    exercises

--- src/main/java/org/launchcode/controllers/PlanetController.java
+++ src/main/java/org/launchcode/controllers/PlanetController.java
@@ -2,13 +2,15 @@ package org.launchcode.controllers;
 
 import org.launchcode.data.Planets;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PlanetController {
 
     @GetMapping("")
-    public String displayIndex(Model model) {
+    public String displayIndex(Model model, Planets planets) {
+
         model.addAttribute("planets", Planets.values());
         return "index";
     }
